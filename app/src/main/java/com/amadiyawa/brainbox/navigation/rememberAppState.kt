485f182c4b6874f658328aeec1f6347b3da9e79a
Amadou Iyawa <amadiyawa@yahoo.com>
2024-04-21 13:09:46 +0100

    Setup App's navigation core

---
+++ app/src/main/java/com/amadiyawa/brainbox/navigation/rememberAppState.kt
@@ -0,0 +1,17 @@
+package com.amadiyawa.brainbox.navigation
+
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun rememberAppState(
+    windowSizeClass: WindowSizeClass,
+    navController: NavHostController = rememberNavController()
+): AppState {
+    return remember(windowSizeClass, navController){
+        AppState(navController)
+    }
+}
